3c62a9024e1d188172289434c931709f8bd3601f
moter <oter.mmurat@gmail.com>
2021-09-25 03:15:58 +0300

    local dynamodb

--- awslambda/app.py
+++ awslambda/app.py
@@ -6,30 +6,37 @@ from flask import Flask, jsonify, make_response, request
 app = Flask(__name__)
 
 
-dynamodb_client = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
 if os.environ.get('IS_OFFLINE'):
-    dynamodb_client = boto3.client(
-        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
-    )
+    print("is offline")
+    dynamodb = boto3.resource('dynamodb', region_name='localhost', endpoint_url='http://localhost:8000')
 
 
-USERS_TABLE = os.environ['USERS_TABLE']
+USERS_TABLE = dynamodb.Table(os.environ['USERS_TABLE'])
 
 
 @app.route('/users/<string:user_id>')
 def get_user(user_id):
-    result = dynamodb_client.get_item(
-        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
+    result = USERS_TABLE.get_item(
+        Key={'userId': user_id}
     )
     item = result.get('Item')
+    print(result)
     if not item:
         return jsonify({'error': 'Could not find user with provided "userId"'}), 404
-
     return jsonify(
-        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
+        {'userId': item.get('userId'), 'name': item.get('name')}
     )
 
+@app.route('/users/', methods=['GET'])
+def get_users():
+    result = USERS_TABLE.scan()
+    print("result: ",result)
+    items = result.get('Items')
+    print(items)
+    
+    return jsonify(items)
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -38,9 +45,7 @@ def create_user():
     if not user_id or not name:
         return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
 
-    dynamodb_client.put_item(
-        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': name}}
-    )
+    USERS_TABLE.put_item(Item={'userId': user_id, 'name': name})
 
     return jsonify({'userId': user_id, 'name': name})
 
